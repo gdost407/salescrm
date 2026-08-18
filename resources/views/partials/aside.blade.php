73fe1672ce5cfbde1c9db955a5efc85ff6eb0628
GDost 407 <gdost407@gmail.com>
2026-08-18 23:25:35 +0530

    Add active menu state detection for all navigation links

--- resources/views/partials/aside.blade.php
+++ resources/views/partials/aside.blade.php
@@ -68,14 +68,14 @@
 
   <ul class="menu-inner py-1">
     <!-- Dashboard -->
-    <li class="menu-item active">
+    <li class="menu-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
       <a href="{{ route('dashboard') }}" class="menu-link">
         <i class="menu-icon tf-icons bx bx-home-circle"></i>
         <div data-i18n="Analytics">Dashboard</div>
       </a>
     </li>
 
-    <li class="menu-item">
+    <li class="menu-item {{ request()->routeIs('sales-kanban') ? 'active' : '' }}">
       <a href="{{ route('sales-kanban') }}" class="menu-link">
         <i class="menu-icon tf-icons bx bx-home-circle"></i>
         <div data-i18n="Analytics">Kanban</div>
@@ -83,24 +83,24 @@
     </li>
 
     <!-- sales -->
-    <li class="menu-item">
+    <li class="menu-item {{ request()->routeIs('sales-*') ? 'active open' : '' }}">
       <a href="javascript:void(0);" class="menu-link menu-toggle">
         <i class="menu-icon tf-icons bx bx-layout"></i>
         <div data-i18n="sales">Sales</div>
       </a>
 
       <ul class="menu-sub">
-        <li class="menu-item">
+        <li class="menu-item {{ request()->routeIs('sales-create-lead') ? 'active' : '' }}">
           <a href="{{ route('sales-create-lead') }}" class="menu-link">
             <div data-i18n="Without menu">Create Lead</div>
           </a>
         </li>
-        <li class="menu-item">
+        <li class="menu-item {{ request()->routeIs('sales-all-list') ? 'active' : '' }}">
           <a href="{{ route('sales-all-list') }}" class="menu-link">
             <div data-i18n="Without navbar">All List</div>
           </a>
         </li>
-        <li class="menu-item">
+        <li class="menu-item {{ request()->routeIs('sales-lead-settings') ? 'active' : '' }}">
           <a href="{{ route('sales-lead-settings') }}" class="menu-link">
             <div data-i18n="Container">Lead Settings</div>
           </a>
@@ -111,23 +111,23 @@
     <!-- <li class="menu-header small text-uppercase">
       <span class="menu-header-text">Pages</span>
     </li> -->
-    <li class="menu-item">
+    <li class="menu-item {{ request()->routeIs('staff-*') ? 'active open' : '' }}">
       <a href="javascript:void(0);" class="menu-link menu-toggle">
         <i class="menu-icon tf-icons bx bx-dock-top"></i>
         <div data-i18n="Account Settings">Staff</div>
       </a>
       <ul class="menu-sub">
-        <li class="menu-item">
+        <li class="menu-item {{ request()->routeIs('staff-create') ? 'active' : '' }}">
           <a href="{{ route('staff-create') }}" class="menu-link">
             <div data-i18n="Account">Create Staff</div>
           </a>
         </li>
-        <li class="menu-item">
+        <li class="menu-item {{ request()->routeIs('staff-manage') ? 'active' : '' }}">
           <a href="{{ route('staff-manage') }}" class="menu-link">
             <div data-i18n="Notifications">Manage Staff</div>
           </a>
         </li>
-        <li class="menu-item">
+        <li class="menu-item {{ request()->routeIs('staff-roles') ? 'active' : '' }}">
           <a href="{{ route('staff-roles') }}" class="menu-link">
             <div data-i18n="Connections">Roles</div>
           </a>
